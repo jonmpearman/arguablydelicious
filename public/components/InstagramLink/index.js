a13b348d659bd76843854fe3d08f7b09dd6c4830
Jon Pearman <jpearman@sbgtv.com>
2019-09-16 23:04:24 -0700

    remove images

--- public/components/InstagramLink/index.js
+++ public/components/InstagramLink/index.js
@@ -4,6 +4,9 @@ import './index.css';
 // <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"         title="Flaticon">www.flaticon.com</a></div>
 export default (props) => {
     return (<div className={'instagramContainer'}>
-                <a href={'https://www.instagram.com/arguablydelicious/'}><img className={'instaIcon'} src={'./images/instagram.svg'}/><div className={'instaHandle'}>{props.title}</div></a>
+                <a href={'https://www.instagram.com/arguablydelicious/'}>
+                    <img className={'instaIcon'} src={'https://drive.google.com/uc?id=1ID2JLo0oUJxzo42yUIyG-ItBVuB4wkhQ'}/>
+                    <div className={'instaHandle'}>{props.title}</div>
+                </a>
             </div>);
 }
